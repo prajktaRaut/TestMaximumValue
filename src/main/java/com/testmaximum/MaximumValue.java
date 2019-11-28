66573e937fae8ff8e6b94a2169a90cbd212cda8c
prajktaRaut <prajktasraut16@gmail.com>
2019-11-28 16:30:33 +0530

    Write a test case to find maximum between three Float values

--- src/main/java/com/testmaximum/MaximumValue.java
+++ src/main/java/com/testmaximum/MaximumValue.java
@@ -16,4 +16,20 @@ public class MaximumValue {
         return null;
 
     }
+
+
+    public Float findMaximumFloatValue(Float a, Float b, Float c) {
+
+        if (a.compareTo(b)>0)
+            return a ;
+
+        if (b.compareTo(c)>0)
+            return b;
+
+        if (c.compareTo(a)>0)
+            return c;
+
+        return null;
+
+    }
 }
--- src/test/java/com/testmaximum/MaximumValueTest.java
+++ src/test/java/com/testmaximum/MaximumValueTest.java
@@ -5,14 +5,23 @@ import org.junit.Test;
 
 public class MaximumValueTest {
 
-    @Test
-    public void givenMethod_FindMaximumInteger_ReturnMaxValue() {
+    MaximumValue maximumValue=new MaximumValue();
 
-        MaximumValue maximumValue=new MaximumValue();
+    @Test
+    public void givenMethod_FindMaximumInteger_ReturnMaxIntegerValue() {
 
         Integer maxValue=maximumValue.findMaximumIntegerValue(40,70,30);
 
         Assert.assertEquals(Integer.valueOf(70),maxValue);
+
+    }
+
+    @Test
+    public void givenMethod_FindMaximumFloat_ReturnMaxFloatValue() {
+
+        Float maxValue=maximumValue.findMaximumFloatValue(0.5f,1.2f,5.3f);
+
+        Assert.assertEquals(Float.valueOf(5.3f),maxValue);
 
     }
 }
