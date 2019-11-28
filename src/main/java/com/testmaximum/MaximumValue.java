0122c8afca4bef156c2f8e05e04ab4c51a5a0999
prajktaRaut <prajktasraut16@gmail.com>
2019-11-28 17:33:29 +0530

    Write a test case to find maximum between any data type value using generics

--- src/main/java/com/testmaximum/MaximumValue.java
+++ src/main/java/com/testmaximum/MaximumValue.java
@@ -1,6 +1,21 @@
 package com.testmaximum;
 
-public class MaximumValue {
+public class MaximumValue<E extends Comparable<E>> {
+
+    public static<E> E FindMaxValueOfAnyType(E a, E b, E c) {
+
+        if (a.toString().compareTo(b.toString())>0)
+            return a ;
+
+        if (b.toString().compareTo(c.toString())>0)
+            return b;
+
+        if (c.toString().compareTo(a.toString())>0)
+            return c;
+
+        return null;
+
+    }
 
     public Integer findMaximumIntegerValue(Integer a, Integer b, Integer c) {
 
--- src/test/java/com/testmaximum/MaximumValueTest.java
+++ src/test/java/com/testmaximum/MaximumValueTest.java
@@ -12,7 +12,6 @@ public class MaximumValueTest {
 
         Integer maxValue=maximumValue.findMaximumIntegerValue(40,70,30);
         Assert.assertEquals(Integer.valueOf(70),maxValue);
-
     }
 
     @Test
@@ -20,7 +19,6 @@ public class MaximumValueTest {
 
         Float maxValue=maximumValue.findMaximumFloatValue(0.5f,1.2f,5.3f);
         Assert.assertEquals(Float.valueOf(5.3f),maxValue);
-
     }
 
     @Test
@@ -28,6 +26,28 @@ public class MaximumValueTest {
 
         String maxValue=maximumValue.findMaximumStringValue("abc","xyz","pqr");
         Assert.assertEquals("xyz",maxValue);
-
     }
+
+    @Test
+    public void givenMethod_FindMaximumValue_ReturnMaxValueOfInteger_UsingGenerics() {
+
+        Integer maxvalue=MaximumValue.FindMaxValueOfAnyType(12,23,34);
+        Assert.assertEquals(Integer.valueOf(34),maxvalue);
+    }
+
+    @Test
+    public void givenMethod_FindMaximumValue_ReturnMaxValueOfFloat_UsingGenerics() {
+
+        Float maxvalue=MaximumValue.FindMaxValueOfAnyType(1.2f,2.3f,3.4f);
+        Assert.assertEquals(Float.valueOf(3.4f),maxvalue);
+    }
+
+    @Test
+    public void givenMethod_FindMaximumValue_ReturnMaxValueOfString_UsingGenerics() {
+
+        String maxvalue=MaximumValue.FindMaxValueOfAnyType("abc","xyz","pqr");
+        Assert.assertEquals("xyz",maxvalue);
+    }
+
+
 }
